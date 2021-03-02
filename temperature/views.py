55b8592b937b40fdffbdd8f7281c435c189a20c4
a <a@localhost.localdomain>
2021-03-01 20:30:46 -0500

    added database stuff and templates

--- temperature/views.py
+++ temperature/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from temperature.models import location,tempEntry
 import random
 
 # Create your views here.
 
 def recent_temp(request):
-    return HttpResponse("the current temperature is {temp:n}".format(temp=random.randint(32,100)))
+    locations=location.objects.values()
+    data=getData(1)
+    print(data)
+    params={'title':'current Locations:','data':data}
+    return render(request, "temperature/current_temp.html", params)
+
+def getData(time):
+    data=[]
+    tempData=tempEntry.objects.all().values().order_by('time')
+    locationsData=location.objects.values()
+    for i in locationsData:
+        data.append([i.get('city'),tempData.filter(place_id=i.get('id')).last()])
+    return data
+
