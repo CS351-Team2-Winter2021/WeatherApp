672182bce3b38e37cd4c90f6316e605d5d9d814e
a <a@localhost.localdomain>
2021-03-08 20:34:17 -0500

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# WeatherApp
-Final project for CS351. Utilizes Raspberry Pi's to track weather data and sends that data to a website.
--- temperature/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class location(models.Model):
-    temp= models.IntegerField()
