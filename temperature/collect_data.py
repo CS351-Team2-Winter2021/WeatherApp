a8618d2995ee36e0b896fbf5ee15b2a8632caa33
Jesse Estes <jestes5111@gmail.com>
2021-03-14 17:08:27 -0400

    Data is now collected every minute as long as the script is running

--- temperature/collect_data.py
+++ temperature/collect_data.py
@@ -3,6 +3,7 @@
 import smbus2
 import bme280
 import requests
+import time
 
 # set i2c port, address
 port = 1
@@ -17,23 +18,27 @@ try:
 except:
     pass
 
-# take a reading, return a compensated_reading object
-data = bme280.sample(bus, address, calibration_parameters)
-
-# convert temperature to fahrenheit
-temperature = (int(data.temperature) * 1.8) + 32
-temperature = int(temperature)
-
-# save the timestamp
-time = data.timestamp
-
-# create a temperature entry
-url = 'http://ec2-18-218-173-23.us-east-2.compute.amazonaws.com/temperature/writetemp/1/' + str(temperature)
-
 # read key.txt to get the key for the Flint node
 with open('key.txt') as f:
     key = f.read().strip()
 
-# send the temperature entry
-dict = {'key':str(key)}
-requests.post(url, dict)
+while True:
+    # take a reading, return a compensated_reading object
+    data = bme280.sample(bus, address, calibration_parameters)
+    
+    # convert temperature to fahrenheit
+    temperature = (int(data.temperature) * 1.8) + 32
+    temperature = int(temperature)
+    
+    # save the timestamp
+    timestamp = data.timestamp
+    
+    # create a temperature entry
+    url = 'http://ec2-18-218-173-23.us-east-2.compute.amazonaws.com/temperature/writetemp/1/' + str(temperature)
+    
+    # send the temperature entry
+    dict = {'key':str(key)}
+    requests.post(url, dict)
+
+    # wait for one minute, then run the loop again
+    time.sleep(60)
