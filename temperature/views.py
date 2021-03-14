5a612be1fb77444ad08ff0d967e88d8535f14992
a <a@localhost.localdomain>
2021-03-14 00:09:20 -0500

    Note

--- temperature/views.py
+++ temperature/views.py
@@ -6,14 +6,25 @@ from plotly.offline import plot
 import time
 from plotly.graph_objs import Scatter
 import random
+import plotly.express as px
+from django.template.loader import render_to_string
 
 # Create your views here.
-
 def recent_temp(request):
     locations=location.objects.values()
-    data=getData(1)
-    print(data)
-    params={'title':'current Locations:','data':data}
+    px.set_mapbox_access_token("")
+    data=location.objects.values()
+    long=[]
+    lat=[]
+    for i in data:
+        l1=i.get('longitude')
+        l2=i.get('latitude')
+        long.append(l1)
+        lat.append(l2)
+    data=getData(0)
+    fig=px.scatter_mapbox(lat=lat, lon=long).write_html("temperature/templates/temperature/map.html")
+    map = render_to_string('temperature/map.html')
+    params={'title':'current Locations:','data':data, 'map':map}
     return render(request, "temperature/current_temp.html", params)
 
 def historical_data(request,ID):
@@ -26,6 +37,7 @@ def getData(time):
     tempData=tempEntry.objects.all().values().order_by('time')
     locationsData=location.objects.values()
     for i in locationsData:
+        print(i.get("city"))
         data.append([i.get('city'),tempData.filter(place_id=i.get('id')).last(),i.get('id')])
     return data
 
